9c55620120362674ec44eb47727b4ebe1d004515
vfsphotos <viniciusfphotographer@gmail.com>
2024-08-05 21:12:30 -0300

    Conexões

--- publication/urls.py
+++ publication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import about, contact, index, post
 
 urlpatterns = [
     path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('post/', post, name='post'),
 ]
--- publication/views.py
+++ publication/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def post(request):
+    return render(request, 'post.html')
